7cc1cf5c536a04bb9d71a28287cbd1bc269a811f
yahirpre <yprenger@ucsc.edu>
2026-05-01 14:08:06 -0700

    collision detection and fish spawns

--- src/Scenes/Level.js
+++ src/Scenes/Level.js
@@ -9,6 +9,10 @@ class Level extends Phaser.Scene {
 
         this.speed = 200; //in pixels/sec
         this.spikeSpeed = 500;
+        this.fishSpeed = 50;
+
+        this.spawnCooldown = 2; //in seconds
+        this.spawnCooldownCounter = 0;
         
     }
 
@@ -17,6 +21,8 @@ class Level extends Phaser.Scene {
         this.load.setPath("./assets/");
         this.load.image("diver", "alienBeige_swim1.png");
         this.load.image("spike", "spike_top.png");
+        this.load.image("greenFish", "fishGreen.png");
+        
     }
 
     create() {
@@ -46,15 +52,46 @@ class Level extends Phaser.Scene {
         my.sprite.spikeGroup.scaleXY(-0.75, -0.75);
         my.sprite.spikeGroup.angle(90);
 
+        //make green fish group
+        my.sprite.greenFishGroup = this.add.group({
+            defaultKey: "greenFish",
+            maxSize: 10,
+            }
+        )
+
+        my.sprite.greenFishGroup.createMultiple({
+            active: false,
+            visible: false,
+            key: my.sprite.greenFishGroup.defaultKey,
+            repeat: my.sprite.greenFishGroup.maxSize-1
+        });
+
+
         // Create the player sprite
         my.sprite.player = this.add.sprite(this.bodyX, this.bodyY, "diver");
         my.sprite.player.setScale(0.75);
         
     }
-w 
+
     update(time, delta) {
         let my = this.my;    // create an alias to this.my for readability
         let dt = delta/1000;
+
+        this.spawnCooldownCounter++;
+
+        //check if spawnCooldown is finished
+        if(this.spawnCooldownCounter > this.spawnCooldown / dt){
+
+            //spawn fish unless all are active
+            let greenFish = my.sprite.greenFishGroup.getFirstDead();
+            if(greenFish != null){ //if all are active, greenFish will be null
+                greenFish.active = true;
+                greenFish.visible = true;
+                greenFish.x = game.config.width + greenFish.displayWidth/2; //set offscreen
+                greenFish.y = Math.random() * game.config.height;
+            }
+            this.spawnCooldownCounter = 0;
+        }
 
         //move up
         if(this.up.isDown){
@@ -95,9 +132,35 @@ w
             }
         }
 
-        //move all spikes forward
-        my.sprite.spikeGroup.incX(this.spikeSpeed*dt);
+        //check fish-spike collision (naive approach)
+        for(let spike of my.sprite.spikeGroup.getChildren()){
+           for(let fish of my.sprite.greenFishGroup.getChildren()){
+                if(fish.active && spike.active && this.collides(fish,spike)){
+                    spike.active = false;
+                    spike.visible = false;
+                    fish.active = false;
+                    fish.visible = false;
+                }
+            }
+        }
 
+        //increment movement of groups
+        my.sprite.spikeGroup.incX(this.spikeSpeed*dt);
+        my.sprite.greenFishGroup.incX(-(this.fishSpeed*dt));
+
+    }
+
+    newWave(){
+        //increase fish speed
+        this.fishSpeed += 10;
+        //increase maxSize of fish groups
+
+    }
+
+    collides(a, b) {
+        if (Math.abs(a.x - b.x) > (a.displayWidth/2 + b.displayWidth/2)) return false;
+        if (Math.abs(a.y - b.y) > (a.displayHeight/2 + b.displayHeight/2)) return false;
+        return true;
     }
 
 }
